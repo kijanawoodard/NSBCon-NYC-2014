4d93b5756126f908665c9a37106d0c59bfa0d5d7
Kijana Woodard <github@wyldeye.com>
2014-09-27 17:59:24 -0500

    more pictures

--- src/EventsFtw/BitwiseCommerce.Billing.Endpoint/EndpointConfig.cs
+++ src/EventsFtw/BitwiseCommerce.Billing.Endpoint/EndpointConfig.cs
@@ -7,14 +7,19 @@ namespace BitwiseCommerce.Billing.Endpoint
 		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
 		can be found here: http://particular.net/articles/the-nservicebus-host
 	*/
+
     public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization
     {
         public void Init()
         {
             Configure.With()
                 .DefaultBuilder()
-                .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.Contains("InternalMessages"))
-                .DefiningEventsAs(t => t.Namespace != null && t.Namespace.Contains("Contracts"))
+                .DefiningCommandsAs(t => 
+                            t.Namespace != null && 
+                            t.Namespace.Contains("InternalMessages"))
+                .DefiningEventsAs(t => 
+                            t.Namespace != null && 
+                            t.Namespace.Contains("Contracts"))
                 .UseInMemoryTimeoutPersister()
                 .InMemorySagaPersister()
                 .InMemorySubscriptionStorage()
